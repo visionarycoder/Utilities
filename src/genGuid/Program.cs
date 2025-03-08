0c5e1478429a34699f7c0d947315e5d0bc1ed249
Ivan Jones <visionarycoder@users.noreply.github.com>
2025-03-07 16:24:33 -0800

    Adding genGuid app

---
+++ src/genGuid/Program.cs
@@ -0,0 +1,20 @@
+﻿using TextCopy;
+
+List<string> exitOptions = ["X", "EXIT", "Q", "QUIT"];
+
+while(true)
+{
+
+    var newGuid = Guid.NewGuid();
+    ClipboardService.SetText(newGuid.ToString("D"));
+
+    Console.WriteLine($"New GUID: {newGuid}");
+    Console.WriteLine("Press Enter to generate another, or type 'X' to quit.");
+
+    var input = Console.ReadLine() ?? string.Empty;
+    if(! string.IsNullOrEmpty(input))
+        continue;
+
+    if(exitOptions.Contains(input.ToUpperInvariant().Trim()))
+        break;
+}
